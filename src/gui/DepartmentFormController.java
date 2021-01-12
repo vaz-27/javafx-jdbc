b7303798bd975191bc164d1600b5644ff80f1269
vaz-27 <martriz4ever@gmail.com>
2021-01-12 20:00:50 -0300

    Salvando um novo departamento

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,17 +3,24 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import db.DbException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
 	
 	private Department entidade;
+	private DepartmentService service;
 
 	@FXML
 	private TextField txtId;
@@ -34,14 +41,41 @@ public class DepartmentFormController implements Initializable {
 		this.entidade = entidade;
 	}
 	
-	@FXML
-	public void onButtonSaveAction() {
-		System.out.println("onButtonSaveAction");
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
 	}
 	
 	@FXML
-	public void onButtonCancelAction() {
+	public void onButtonSaveAction(ActionEvent event) {
+		if (entidade == null) {
+			throw new IllegalStateException("Entidade está nula");
+		}
+		if (service == null) {
+			throw new IllegalStateException("Servico nulo");
+		}
+		
+		try {
+			entidade = getFormData();
+			service.saveOrUpdate(entidade);
+			Utils.currentStage(event).close();
+		} catch(DbException e) {
+			Alerts.showAlert("Erro ao salvar Departamento", null, e.getMessage(), AlertType.ERROR);
+		}
+	}
+	
+	private Department getFormData() {
+		Department obj = new Department();
+		
+		obj.setId(Utils.tryparsetoInt(txtId.getText()));
+		obj.setName(txtNome.getText());
+		
+		return obj;
+	}
+
+	@FXML
+	public void onButtonCancelAction(ActionEvent event) {
 		System.out.println("onButtonCancelAction");
+		Utils.currentStage(event).close();
 	}
 	
 	@Override
